313ae4b711e29655103ff2359c8f3a99726cebe0
0Xbug1971 <72446101+0Xbug1971@users.noreply.github.com>
2020-10-06 17:41:38 +0600

    Create demo.js
    creat a file

---
+++ demo.js
@@ -0,0 +1,11 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+// This can be used to set the Particles Effects. Check README for more details!
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
