54f45928b2dfba0f60cd7e67df1582a809b7486c
Tamjid <45327979+tamjid0x01@users.noreply.github.com>
2021-10-09 20:26:10 +0600

    Create app.js
    Create app.js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var App = Composer.Model.extend({
+	connected: function() {
+		return turtl.core.send('app:connected');
+	},
+
+	wipe_user_data: function() {
+		return turtl.core.send('app:wipe-user-data');
+	},
+
+	wipe_app_data: function() {
+		return turtl.core.send('app:wipe-app-data');
+	},
+
+	set_api_config: function(config) {
+		return turtl.core.send('app:api:set-config', config);
+	},
+
+	get_api_config: function() {
+		return turtl.core.send('app:api:get-config');
+	},
+
+
+
+	shutdown: function() {
+		return turtl.core.send('app:shutdown');
+	},
+});
